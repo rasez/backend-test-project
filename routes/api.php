bc0c5b7e9c84963d9cef2ce1e51141c18b0b36e6
reza <r.abbasi@irantalent.com>
2020-10-03 04:02:35 +0330

    jwt authentication

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['namespace' => 'Api\V1', 'prefix' => 'v1',], function () {
+    Route::group(['namespace' => 'Auth'], function () {
+        Route::post('login', 'JwtController@login');
+        Route::post('register', 'JwtController@register');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+        Route::group(['middleware' => 'auth:api'], function () {
+            Route::get('logout', 'JwtController@logout');
+        });
+    });
+
+    Route::get('products', 'ProductController@index');
 });
